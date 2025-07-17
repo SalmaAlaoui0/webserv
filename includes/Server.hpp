14aa24730dfda7919d490c97d7a7bcb5e09cd696
Wafaa Zahir <wzahir@c3r8p4.1337.ma>
2025-07-17 22:58:41 +0100

    Phase 3: Sockets & Polling finish

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: wzahir <wzahir@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 16:30:18 by wzahir            #+#    #+#             */
-/*   Updated: 2025/07/17 18:33:48 by wzahir           ###   ########.fr       */
+/*   Updated: 2025/07/17 22:46:23 by wzahir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,18 @@
 #include <fcntl.h>
 #include "ServerConfig.hpp"
 #include "EpollManager.hpp"
+#include "Request.hpp"
 #include "Client.hpp"
 
 
 struct ServerConfig;
-
+class EpollManager;
 class Server
 {
     private:
         std::vector<ServerConfig> _configs;
         std::vector<int> serverSockets;
-        //std::map<int, Client> clients;
-        std::vector<int> clients;
+        std::map<int, Client> clients;
     public:
         Server(const std::vector<ServerConfig>& configs);
        ~Server();
@@ -45,8 +45,11 @@ class Server
         int creatServerSocket(const std::string &ip, int port);
         void run();
         bool isServerSocket(int fd) const;
-        void acceptNewClient(int listenFd,  EpollManager &EpollManager);
+        void acceptNewClient(int listenFd, EpollManager &epollManager);
+        void handleClient(int clientFd, EpollManager &epollManager);
         void closeClient(int fd, EpollManager &epollManager);
+        void checkTimeout(std::map<int, Client> &clients, EpollManager &epoll);
+        std::string readRequest(int clientFd, EpollManager &epollManager);
         
         class socketException : public std::exception 
         {
