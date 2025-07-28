c8064b9f97d8b63aba25c120d9835b211cd85110
salaoui <salaoui@c3r8p5.1337.ma>
2025-07-28 13:56:43 +0100

    Get method still has somework to do

--- testingdirectories.cpp
+++ testingdirectories.cpp
@@ -53,3 +53,67 @@ int main() {
     return 0;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+/// AUTOINDEX IS ON ---->
+
+// Helper function to generate autoindex HTML
+// std::string generate_autoindex_html(std::string path, std::string uri) {
+//     std::stringstream html;
+//     html << "<html><head><title>Index of " << uri << "</title></head><body>";
+//     html << "<h1>Index of " << uri << "</h1><ul>";
+
+//     DIR* dir = opendir(path.c_str());
+//     if (!dir) {
+//         html << "<li>Unable to open directory</li>";
+//     } else {
+//         struct dirent* entry;
+//         while ((entry = readdir(dir)) != NULL) {
+//             std::string name = entry->d_name;
+//             // Skip '.' to avoid self link loops, optional
+//             if (name == ".") continue;
+
+//             std::string link = uri;
+//             if (!link.empty() && link[link.size() - 1] != '/')
+//     			link += "/";
+//             link += name;
+
+//             html << "<li><a href=\"" << link << "\">" << name << "</a></li>";
+//         }
+//         closedir(dir);
+//     }
+
+//     html << "</ul></body></html>";
+//     return html.str();
+// }
+
+// // Function to send autoindex HTML response
+// std::string serve_autoindex_listing(int clientFd, std::string requested_path, std::string uri) {
+//     std::string body = generate_autoindex_html(requested_path, uri);
+
+//     std::stringstream response;
+//     response << "HTTP/1.1 200 OK\r\n";
+//     response << "Content-Type: text/html\r\n";
+//     response << "Content-Length: " << body.size() << "\r\n";
+//     response << "Connection: close\r\n";
+//     response << "\r\n";
+//     response << body;
+
+//     // send the response string via your socket to clientFd
+//     // You probably have a send function; let's say:
+//     send(clientFd, response.str().c_str(), response.str().size(), 0);
+
+//     return ""; // or some status string
+// }
+
