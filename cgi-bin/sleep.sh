e402720c5e501a735cc8f0207951ad3230a687d5
Mouna Labyed <mlabyed@c4r3p3.1337.ma>
2025-10-16 19:12:35 +0100

    hiiii

--- cgi-bin/sleep.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# CGI requires a header before any output
-echo "Content-Type: text/plain"
-echo
-
-# Simulate a very long-running CGI
-sleep 1222222222
-echo "This should never be reached if timeout works."
